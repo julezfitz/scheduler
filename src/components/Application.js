5f243b903a0a59f7a6e4877026da83aac81dc008
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-11-15 13:22:50 -0330

    changed usestate to an object containing day, appointments and days

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,48 +5,56 @@ import Appointment from "./Appointment/index.js";
 import axios from "axios";
 import { getAppointmentsForDay } from "helpers/selectors";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 export default function Application(props) {
 
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     const daysUrl = `http://localhost:8001/api/days`;
@@ -56,7 +64,7 @@ export default function Application(props) {
     });
   }, []);
 
-  const appointmentsDisplay = Object.values(appointments).map(appointment => {
+  const appointmentsDisplay = Object.values(state.appointments).map(appointment => {
 
     return (
       <Appointment
@@ -67,8 +75,6 @@ export default function Application(props) {
   })
 
   appointmentsDisplay.push(<Appointment key="last" time="5pm" />);
-
-  const [day, setDay] = useState("Monday");
 
   return (
     <main className="layout">
@@ -81,8 +87,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
