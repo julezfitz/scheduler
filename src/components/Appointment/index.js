183f1cf409304970fa5c1a860fbb9d6ce2f0af70
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-11-19 21:20:27 -0330

    add confirm state to delete sequence

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import Header from "./Header.js";
 import Show from "./Show.js";
 import Empty from "./Empty.js";
 import Status from "./Status.js";
+import Confirm from "./Confirm.js";
 import Form from "./Form.js";
 import React, { Fragment } from 'react'
 import { useVisualMode } from "../../hooks/useVisualMode";
@@ -12,6 +13,8 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+
 
 export default function Appointment(props) {
 
@@ -31,6 +34,10 @@ export default function Appointment(props) {
     props.onDelete(props.id).then(() => { transition(EMPTY) })
   }
 
+  const confirmDelete = function () {
+    transition(CONFIRM);
+  }
+
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY)
 
   console.log(props.interview);
@@ -40,13 +47,21 @@ export default function Appointment(props) {
       <Header time={props.time} />
       { mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SAVING && <Status message={"Saving"} />}
+      {mode === CONFIRM && (
+      <Confirm 
+      message={"Are you sure you would like to delete?"}
+      onConfirm={deleteItem}
+      onCancel={() => back()}
+      />
+      )
+      }
       {mode === DELETING && <Status message={"Deleting"} />}
       { mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           interviewers={props.interviewers}
-          onDelete={deleteItem}
+          onDelete={confirmDelete}
         />
       )
       }
